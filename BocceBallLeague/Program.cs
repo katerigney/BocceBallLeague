d030a3ba30e0b825ab274971f397da0ee71eb8c8
Rigney <krigney@proforma.com>
2018-04-17 16:36:56 -0400

    Completed 'View past Ga mes'functionality.

--- BocceBallLeague/Program.cs
+++ BocceBallLeague/Program.cs
@@ -20,11 +20,21 @@ namespace BocceBallLeague
             var upcoming = db.Games.Where(game => game.Date > DateTime.Today);
             foreach (var game in upcoming)
             {
-                Console.WriteLine($"There is an upcoming game on {game.Date}.");
+                Console.WriteLine($"There is an upcoming game on {game.Date.ToString("MMMM dd, yyyy")}.");
                 
             }
 
+            Console.WriteLine($"--------------------------------------------");
+
+
             //Past games
+
+            var past = db.Games.Where(game => game.Date < DateTime.Today);
+            foreach (var game in past)
+            {
+                Console.WriteLine($"Games were played on {game.Date}.");
+
+            }
 
 
 
