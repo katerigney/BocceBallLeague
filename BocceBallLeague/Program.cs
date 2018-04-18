c5e503effb5bfe99823ef7129b3bde2726ae236f
Rigney <krigney@proforma.com>
2018-04-17 20:40:52 -0400

    added structure for adventure mode and completed 'User Add Team' functionality.

--- BocceBallLeague/Program.cs
+++ BocceBallLeague/Program.cs
@@ -13,64 +13,139 @@ namespace BocceBallLeague
         static void Main(string[] args)
         {
             var db = new DataContext();
+            Console.WriteLine("Tampa Bay Bocce Ball League");
+            Console.WriteLine("---------------------------");
 
+
+            //EXPLORER MODE/////////////////////////////////////
+
+
+            //-----------------------------------------------      
+            //All the Players and what team they are on
+
+            Console.WriteLine("LEAGUE PLAYER INFORMATION");
+            var players = db.Players.Include(i => i.Team);
+            foreach (var person in players)
+            {
+                Console.WriteLine($"{person.FullName} plays for the {person.Team.Mascot}");
+            }
+
+            Console.WriteLine($"--------------------------------------------");
+
+
+            //-----------------------------------------------
+            //Past games
+
+            Console.WriteLine("PREVIOUS LEAGUE GAMES");
+            var past = db.Games.Where(game => game.Date < DateTime.Today);
+            foreach (var game in past)
+            {
+                Console.WriteLine($"Games were played on {game.Date.ToString("MMMM dd, yyyy")}.");
+            }
+
+            Console.WriteLine($"--------------------------------------------");
+
+
+            //-----------------------------------------------
             //All the teams, with their win / loss record
 
+            Console.WriteLine("LEAGUE WIN/LOSS RECORD");
             var teams = db.Teams;
             foreach (var team in teams)
             {
                 Console.WriteLine($"The {team.Mascot} have won {team.Wins} times and lost {team.Losses} times.");
             }
 
-            //All the Players and what team they are on
-
-            var players = db.Players.Include(i => i.Team);
-            
-            foreach (var person in players)
-            {
-                Console.WriteLine($"{person.FullName} plays for the {person.Team.Mascot}");
-            }
-
-
-            //All the Upcoming games(games who Date Happened is in the future)
-
-            var upcoming = db.Games.Where(game => game.Date > DateTime.Today);
-            foreach (var game in upcoming)
-            {
-                Console.WriteLine($"There is an upcoming game on {game.Date.ToString("MMMM dd, yyyy")}.");     
-            }
-
             Console.WriteLine($"--------------------------------------------");
 
 
-            //Past games
+            //-----------------------------------------------
+            //All the Upcoming games(games who Date Happened is in the future)
 
-            var past = db.Games.Where(game => game.Date < DateTime.Today);
-            foreach (var game in past)
+            Console.WriteLine("UPCOMING LEAGUE GAMES");
+            var upcoming = db.Games.Where(game => game.Date > DateTime.Today);
+            foreach (var game in upcoming)
             {
-                Console.WriteLine($"Games were played on {game.Date.ToString("MMMM dd, yyyy")}.");
-
+                Console.WriteLine($"There is an upcoming game on {game.Date.ToString("MMMM dd, yyyy")}.");
             }
 
+            Console.WriteLine($"--------------------------------------------");
+            Console.WriteLine($"--------------------------------------------");
 
 
-            /*var firstTeam = new Model.Teams
+            //ADVENTURE MODE/////////////////////////////////////
+
+
+            var programRunning = true;
+            while (programRunning)
             {
-                Mascot = "Bradenton Manatees",
-                Colors = "Aqua/Blue",
-                Records = new Model.Record()
+                Console.WriteLine("Select your next step: Add new (team) to the league, Add a (player) to the league, (Schedule) a game, (Update) game scores, or (Exit)");
+                var response = Console.ReadLine().ToLower();
+
+                if (response == "team")
                 {
-                    Wins = 0,
-                    Losses=9
+                    var addTeam = true;
+                    while (addTeam)
+                    {
+                        Console.WriteLine("Please enter team mascot/name:");
+                        var newTeamMascot = Console.ReadLine();
+                        Console.WriteLine("Please enter team colors:");
+                        var newTeamColors = Console.ReadLine();
+
+                        var newTeam = new Model.Teams
+                        {
+                            Mascot = newTeamMascot,
+                            Colors = newTeamColors,
+                        };
+                        db.Teams.Add(newTeam);
+                        db.SaveChanges();
+                        Console.WriteLine($"{newTeam.Mascot} added to the Tampa Bay Bocce Ball League!");
+                        Console.WriteLine("Do you want to enter another team? (Y) or (N)");
+                        var reply = Console.ReadLine().ToLower();
+                        if (reply == "n")
+                            {
+                                addTeam = false;
+                            }
+                    }
+                    
+
+
+
                 }
-            };
-            db.Teams.Add(firstTeam);
-            db.SaveChanges();*/
+                else if (response == "player")
+                {
+                    Console.WriteLine("Select: (View) win/loss records or (Add) a team to the league");
+
+                }
+                else if (response == "schedule")
+                {
+                    Console.WriteLine("Select: (View) win/loss records or (Add) a team to the league");
+
+                }
+                else if (response == "update")
+                {
+
+                }
+                else
+                {
+                    programRunning = false;
+                }
 
 
 
 
-            Console.ReadLine();
+
+                // Allow Julia(the user) to create new players, and add them to a team
+                //Allow Julia to create new teams
+                //Allow Julia to schedule games only in the future
+                // Allow Julia to enter in the score of a game(only in the past)
+
+
+
+
+
+                
+            }
         }
     }
 }
